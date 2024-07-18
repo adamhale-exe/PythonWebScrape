9e76f0f3ad017dc80cb6889f14aea8841ab8d382
adamhale-exe <adam.hale92@googlemail.com>
2024-07-18 10:44:38 +0100

    cleared linting errors

--- function_builds/assembly_selector.py
+++ function_builds/assembly_selector.py
@@ -1,28 +1,29 @@
+"""Module to automate user actions."""
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
-url = 'http://localhost:5500'
+URL = 'http://localhost:5500'
 
-def assemblySelector():
+def assembly_selector():
+    """Module to build and test a function that deals with a popup."""
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, slow_mo=50)
         page = browser.new_page()
-        page.goto(url)
+        page.goto(URL)
 
         html = page.query_selector('select[name="contentPanel:aggregates"]').inner_html()
         soup = BeautifulSoup(html, 'html.parser')
 
-        optionArray = []
+        option_array = []
 
         for i in soup.findChildren('option'):
-            optionArray.append(i.get_text())
+            option_array.append(i.get_text())
             print(i.get_text)
-        
-        print(optionArray)
-        for index, item in enumerate(optionArray[1:]):
+        print(option_array)
+        for index, item in enumerate(option_array[1:]):
             print(index+1)
             page.locator('select[name="contentPanel:aggregates"]').select_option(index=index+1)
             page.pause()
         browser.close()
 
-main()
+assembly_selector()
